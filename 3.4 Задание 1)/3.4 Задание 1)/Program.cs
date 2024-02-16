d8b3bfb3541c519f82649516bcfcafa1eaa46656
Piholot2K (daniilhonda) <69081378+crokodan@users.noreply.github.com>
2024-02-16 18:56:24 +0400

    Add files via upload

---
+++ 3.4 Задание 1)/3.4 Задание 1)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._4_Задание_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0.1;
+            double y;
+            for(double i = x; i <= 3.0; i+= 10) 
+            { 
+                y = Math.Pow(i,2) + 4 * Math.Sin(Math.PI * i);
+                Console.WriteLine(y);
+            }
+            Console.ReadKey();
+        }
+    }
+}
